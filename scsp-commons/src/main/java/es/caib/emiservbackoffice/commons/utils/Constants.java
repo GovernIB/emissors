42c35519584d9a3e7b13a3220eb6cf57bc7c26dd
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2021-12-16 11:07:59 +0100

    Refactoritzacio commons #1

---
+++ scsp-commons/src/main/java/es/caib/emiservbackoffice/commons/utils/Constants.java
@@ -0,0 +1,21 @@
+package es.caib.emiservbackoffice.commons.utils;
+
+
+/**
+ * Constants emprades dins tota l'aplicació.
+ *
+ * @author anadal
+ * @author areus
+ */
+public interface Constants {
+
+    /**
+     * Constant pel rol d'Administrador
+     */
+    String EBO_ADMIN = "EBO_ADMIN";
+
+    /**
+     * Constant pel rol d'Usuari
+     */
+    String EBO_USER = "EBO_USER";
+}
